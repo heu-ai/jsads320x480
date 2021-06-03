10f2e325a87da56de21a8e5a1bf19ca78b4fd2c9
Siddhesh Ghadi <30323015+Sid072504@users.noreply.github.com>
2021-06-03 18:41:40 +0530

    updated code

--- app.js
+++ app.js
@@ -15,15 +15,13 @@ var clickset = 0
 var bigbtn = 0
 
 // Opening the 1st Image
-openfile(imges[0])
+openfile(imges[0]);
 btnsOff();
 
 // adding Event Listener
 document.getElementById("output").addEventListener('click', function() {
                     //console.log('Click');
                     clickpattern();});
-
-
 
 // Opening a  file
 function openfile(filepath){
@@ -32,15 +30,15 @@ function openfile(filepath){
 };
 
 // click count but it refreshes again as per page loads
-function clicksnum(){
-            console.log("Clicked Now !!!!!");
+function APICall(n){
+            console.log("Clicked Now !!!!!", n);
 };
 
 function imgoff(){
 try{
     var img = document.getElementById("back");
     img.style = "display:none !important";
-    console.log(img);
+    //console.log(img);
     }
 catch(e){
         console.log("No img = ", e);
@@ -57,43 +55,43 @@ for (i=0; i<btns.length; i++){
 };
 
 function multiplebtns(id){
-var btn = document.getElementById(id);
-btn.className = "selected";
+    var btn = document.getElementById(id);
+    btn.className = "selected";
 };
 
 function btnfunc(op){
-//    var img= document.getElementById("back");
+    //var img= document.getElementById("back");
 //    img.src = "320X480V2/CTA_VOLTAR.png";
 //    img.style= "height: 30px; width: 77px; position: absolute; top: 30px;left: 36px;";
-//    img.onclick = function(){btnsOff(); imgoff();op.src=imges[0]; };
+//    img.onclick = function(){btnsOff(); imgoff();op.src=imges[0]; APICall(1);};
 
     var left1 = document.getElementById("left1");
-    left1.style = "position: absolute; top: 260px; left: 33px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
-    left1.onclick = function(){multiplebtns("left1");};
+    left1.style = "position: absolute; top: 180px; left: 33px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
+    left1.onclick = function(){multiplebtns("left1"); APICall(2);};
 
     var right1 = document.getElementById("right1");
-    right1.style = "position: absolute; top: 260px; left: 167px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
-    right1.onclick = function(){multiplebtns("right1");};
+    right1.style = "position: absolute; top: 180px; left: 167px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
+    right1.onclick = function(){multiplebtns("right1");  APICall(3);};
 
     var left2 = document.getElementById("left2");
-    left2.style = "position: absolute; top: 324px; left: 33px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
-    left2.onclick = function(){multiplebtns("left2");};
+    left2.style = "position: absolute; top: 243px; left: 33px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
+    left2.onclick = function(){multiplebtns("left2");  APICall(4);};
 
     var right2 = document.getElementById("right2");
-    right2.style = "position: absolute; top: 324px; left: 167px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
-    right2.onclick = function(){multiplebtns("right2");};
+    right2.style = "position: absolute; top: 243px; left: 167px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
+    right2.onclick = function(){multiplebtns("right2");  APICall(5);};
 
     var left3 = document.getElementById("left3");
-    left3.style = "position: absolute; top: 386px; left: 33px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
-    left3.onclick = function(){clickset = 3; multiplebtns("left3");};
+    left3.style = "position: absolute; top: 306px; left: 33px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
+    left3.onclick = function(){clickset = 3; multiplebtns("left3");  APICall(6);};
 
     var right3 = document.getElementById("right3");
-    right3.style = "position: absolute; top: 386px; left: 167px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
-    right3.onclick = function(){clickset = 5; multiplebtns("right3");};
+    right3.style = "position: absolute; top: 306px; left: 167px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
+    right3.onclick = function(){clickset = 5; multiplebtns("right3");  APICall(7);};
 
     var bigbtn = document.getElementById("bigbtn");
-    bigbtn.style = "position: absolute; top: 475px; left: 52px; height: 65px; background: transparent; width: 229px; border-radius: 2%; border:1px solid transparent";
-    bigbtn.onclick = function(){btnsOff(); imgoff(); bigbtn = 4; if (clickset==3){op.src = imges[2]; } else if (clickset==5){op.src = imges[4];} else {op.src = imges[3];}};
+    bigbtn.style = "position: absolute; top: 395px; left: 52px; height: 65px; background: transparent; width: 229px; border-radius: 2%; border:1px solid transparent";
+    bigbtn.onclick = function(){btnsOff(); imgoff();  APICall(8); bigbtn = 4; if (clickset==3){op.src = imges[2]; } else if (clickset==5){op.src = imges[4];} else {op.src = imges[3];}};
 }
 
 
@@ -113,6 +111,7 @@ function clickpattern(){
     {
         //console.log("Clicked to move 1");
         op.src = imges[1];
+        APICall(0);
         //console.log("Image Changed!!!...", op.src);
         btnfunc(op);
     }
@@ -126,6 +125,7 @@ function clickpattern(){
     {
         btnsOff();
 	    imgoff();
+	    APICall(9);
         op.src = imges[5];
 	    ImageMaplast(op);
         //console.log("Image Changed!!!...", op.src);
@@ -136,6 +136,7 @@ function clickpattern(){
 		 op.src = imges[0];
 		 btnsOff();
 		 imgoff();
+		  APICall(10);
 		 // window.location.href = "https://www.google.com/maps/search/oboticario";
         //console.log("Image Changed!!!...", op.src);
     }
@@ -147,15 +148,15 @@ function ImageMaplast(op) {
     var area = document.getElementById("maps");
     //area.onmouseover = function(){console.log("leftkey");};
     area.shape = "rect";
-    area.coords = "49,301,273,360";
+    area.coords = "84,417,643,577";
    // area.onmouseover = function(){console.log("left");};
-    area.onclick = function(){ window.location.href = "https://www.google.com/maps/search/oboticario"; };
+    area.onclick = function(){  APICall(11); window.location.href = "https://www.google.com/maps/search/oboticario"; };
 
     var area = document.getElementById("backbtn");
     area.shape = "rect";
-    area.coords = "50,377,271,435";
+    area.coords = "84,602,644,761";
    //area.onmouseover = function(){console.log("bigbtn");};
-    area.onclick = function(){ btnsOff(); imgoff(); op.src = imges[0]; window.location.href = ""; };
+    area.onclick = function(){ btnsOff(); imgoff();  APICall(12); op.src = imges[0]; window.location.href = ""; };
  };
 
 // Finding a Position of Mouse Click
@@ -182,9 +183,6 @@ function GetCoordinates(e){
   var PosY = 0;
   var ImgPos;
   ImgPos = FindPosition(output);
-  
-  
-  
   
   if (!e) var e = window.event;
   if (e.pageX || e.pageY)
